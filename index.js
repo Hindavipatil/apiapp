9d27d7289cdb87698399be015a7f1e70d57f882d
Hindavipatil <hindavipatil70@gmail.com>
2025-07-03 20:28:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
